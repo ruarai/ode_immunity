a1c5fed98675efbe07a3a775ff7a8226b33b967b
Ruarai Tobin <ruarai.tobin@unimelb.edu.au>
2024-12-06 11:47:41 +1100

    Update unit tests

--- src/unit_tests.jl
+++ src/unit_tests.jl
@@ -14,8 +14,6 @@ model_params = make_model_parameters(
     @test all(sum(model_params.M, dims = 1) .>= 1-1e-10)
     @test all(model_params.M .>= 0)
 
-    # @test model_params.M[15] == cdf.(c_jump_dist, log10(model_params.c_levels[16])) - cdf.(c_jump_dist, log10(model_params.c_levels[15]))
-
     # Strata variables
     @test all(model_params.c_levels .>= 0) && all(model_params.p_acq .<= 1)
     @test all(model_params.p_acq .>= 0) && all(model_params.p_acq .<= 1)
@@ -27,13 +25,12 @@ ode_sparsity = ode_get_sparsity(model_params)
 ode_solution = @time ode_solve(model_params, n_days, n_inf_0, ode_sparsity)
 
 t = 1:n_days
-sol_I[:, :] = ode_solution(t)[ode_ix(c_sus, 1:model_params.S, model_params.S), :]'
-sol_S[:, :] = ode_solution(t)[ode_ix(c_inf, 1:model_params.S, model_params.S), :]'
+sus, inf, inc = get_results(ode_solution, t, model_params)
 
-@testset "Solving" begin
-    @test all(sol_S .>= 0) && all(sol_S .<= 1)
-    @test all(sol_I .>= 0) && all(sol_I .<= 1)
+@testset "Model result sanity" begin
+    @test all(sus .>= 0) && all(sus .<= 1)
+    @test all(inf .>= 0) && all(inf .<= 1)
+    @test all(inc .>= 0) && all(inc .<= 1)
 
-    @test all(sum(sol_S .+ sol_I, dims = 2) .<= 1.0000001)
-    @test all(sum(sol_S .+ sol_I, dims = 2) .>= 0.0)
+    @test all(sum(sus, dims = 2) .+ inf .<= 1.0000001)
 end
